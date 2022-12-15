1cb19f8cb048b9ea71a52e5cf683d3b6c07dfcb1
Natalie <64873273+DoubleSnapps@users.noreply.github.com>
2022-12-15 15:51:13 -0600

    deleted all my code i made at school cause i can do it better faster harder at home
    she calls me goofy

--- Assets/PageManager.cs
+++ Assets/PageManager.cs
@@ -6,17 +6,21 @@ using TMPro;
 
 public class PageManager : MonoBehaviour
 {
-    public GameObject expanded;
     public GameObject expandedRed;
+    public GameObject chooserRed;
     public GameObject expandedBlue;
+    public GameObject chooserBlue;
+    public GameObject notes;
     public GameObject logo;
     public bool trueIsBlue;
     [System.Serializable]
     public class Page {
         public GameObject gameObject;
         public string title;
-        public bool choose = true;
+        public bool hasChoose = false;
         public bool hasLogo = false;
+        public bool hasExpanded = false;
+        public bool hasNotes = false;
     }
     public Transform tile;
     public int page = 0;
@@ -31,39 +35,58 @@ public class PageManager : MonoBehaviour
     }
 
     // it *supposed* to detect when the chooser is yesed
-    public void expander(Page page) {
-        if (page.choose){
-            expanded.SetActive(false);
+    public void pageAttributes(Page page) {
+        // if notes on page then exist 
+        if (page.hasNotes){
+           notes.gameObject.SetActive(true); 
+        } else {
+            notes.gameObject.SetActive(false);
         }
-        else {
-            // i would by lying if I said that I understand what rudy made
-            bool random = Random.Range(0, 2) == 0 ? false : true;
-            expandedBlue.gameObject.SetActive(random);
-            expandedRed.gameObject.SetActive(!random);
+
+        // if title then existance (futile and SAD)
+        if (page.hasLogo) {
+            logo.gameObject.SetActive(true);
+        } else {
+            logo.gameObject.SetActive(false);
         }
-        
-        if (trueIsBlue){
+
+        // if chooser then existance (hey if any lesbians are reading my code comments my number is 425553920-
+        if (page.hasChoose) {
+            chooserBlue.gameObject.SetActive(true);
+            chooserRed.gameObject.SetActive(true);  
+        } else {
+            logo.gameObject.SetActive(false);
+        }
+
+        // the wide if red or is blue or isnt??? Huwooga.
+        if(page.hasExpanded && trueIsBlue){
             expandedBlue.gameObject.SetActive(true);
             expandedRed.gameObject.SetActive(false);
-        }
-        else {
-            expandedBlue.gameObject.SetActive(false);
+        } else if (page.hasExpanded && !trueIsBlue) {
             expandedRed.gameObject.SetActive(true);
+            expandedBlue.gameObject.SetActive(false);
+        } else {
+            expandedBlue.gameObject.SetActive(false);
+            expandedRed .gameObject.SetActive(false);
         }
-        logo.SetActive(page.hasLogo);
-    }   
-     
+
+
+
+
+    }
+
     // using on click, the "page" will change
-    public void Goto(int newPage) {
+    public void Goto(int newPage)
+    {
         fakeScene[page].gameObject.SetActive(false);
         fakeScene[newPage].gameObject.SetActive(true);
         page = newPage;
         title.text = fakeScene[page].title;
-        expander(fakeScene[page]);
-        expanded.gameObject.SetActive(false);
-        expanded.SetActive(false);
+        pageAttributes(fakeScene[page]);
+
     }
-  
+
+
     // sets wide to red when applicable 
     public void choseRed(){
         trueIsBlue = false;
