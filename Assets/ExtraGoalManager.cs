cbc4f1983cecf48e66a6aa4ce3a6728fe211d8de
Natalie <64873273+DoubleSnapps@users.noreply.github.com>
2022-12-16 00:00:27 -0600

    i lied
    its tomorrow

---
+++ Assets/ExtraGoalManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtraGoalManager : MonoBehaviour
+{
+    public int challangeProgress;
+
+    public void setProgress(int value) {
+        challangeProgress = value;
+    }
+}
