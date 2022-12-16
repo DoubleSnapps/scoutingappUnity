29653c1efde7ada7ffce35b78e0e69a1b2ec08df
Rubyboat <rudy@rudysoliz.com>
2022-12-15 23:16:06 -0600

    Markers

--- Assets/AnimationPlay.cs
+++ Assets/AnimationPlay.cs
@@ -1,7 +1,19 @@
 using UnityEngine;
 public class AnimationPlay : MonoBehaviour
 {
+    public bool is_toggled = false;
+    public string toggle1 = "";
+    public string toggle2 = "";
+
     public void PlayAnimaton(string name) {
+        print(name);
         GetComponent<Animation>().Play(name);
+    }
+
+
+    public void PlayToggle()
+    {
+        is_toggled = !is_toggled;
+        GetComponent<Animation>().Play(is_toggled ? toggle1 : toggle2);
     }
 }
--- Assets/PageManager.cs
+++ Assets/PageManager.cs
@@ -6,74 +6,105 @@ using TMPro;
 
 public class PageManager : MonoBehaviour
 {
+    // GameObjects for the expanded red, blue, and chooser versions of both colors
     public GameObject expandedRed;
     public GameObject chooserRed;
     public GameObject expandedBlue;
     public GameObject chooserBlue;
+    // GameObject for the notes section
     public GameObject notes;
+    // GameObject for the logo
     public GameObject logo;
+    // Boolean indicating whether the "wide" version is currently set to blue or not
     public bool trueIsBlue;
+
+    // Page class to store information about each page in the fakeScene list
     [System.Serializable]
-    public class Page {
+    public class Page
+    {
+        // GameObject for the page
         public GameObject gameObject;
+        // Title of the page
         public string title;
+        // Boolean indicating whether the page has a chooser
         public bool hasChoose = false;
+        // Boolean indicating whether the page has a logo
         public bool hasLogo = false;
+        // Boolean indicating whether the page has an expanded version
         public bool hasExpanded = false;
+        // Boolean indicating whether the page has notes
         public bool hasNotes = false;
     }
+
+    // Transform for the tile
     public Transform tile;
+    // Integer to store the current page
     public int page = 0;
+    // TextMeshProUGUI for the title of the page
     public TextMeshProUGUI title;
+    // List of Page objects representing the fake scene
     public List<Page> fakeScene = new List<Page>();
 
-    void Start() {
-        foreach (var page in fakeScene){
+    void Start()
+    {
+        // Set all pages in the fakeScene list to inactive
+        foreach (var page in fakeScene)
+        {
             page.gameObject.SetActive(false);
         }
+        // Go to the first page
         Goto(0);
     }
 
-    // it *supposed* to detect when the chooser is yesed
-    public void pageAttributes(Page page) {
-        // if notes on page then exist 
-        if (page.hasNotes){
-           notes.gameObject.SetActive(true); 
-        } else {
+    // Method to apply page attributes, such as whether the page has notes, a logo, a chooser, or an expanded version
+    public void pageAttributes(Page page)
+    {
+        // If the page has notes, set the notes GameObject to active
+        if (page.hasNotes)
+        {
+            notes.gameObject.SetActive(true);
+        }
+        else
+        {
+            // Otherwise, set the notes GameObject to inactive
             notes.gameObject.SetActive(false);
         }
 
-        // if title then existance (futile and SAD)
-        if (page.hasLogo) {
+        // If the page has a logo, set the logo GameObject to active
+        if (page.hasLogo)
+        {
             logo.gameObject.SetActive(true);
-        } else {
+        }
+        else
+        {
+            // Otherwise, set the logo GameObject to inactive
             logo.gameObject.SetActive(false);
         }
 
-        // if chooser then existance (hey if any lesbians are reading my code comments my number is 425553920-
-        if (page.hasChoose) {
+        // If the page has a chooser, set both chooser GameObjects to active
+        if (page.hasChoose)
+        {
             chooserBlue.gameObject.SetActive(true);
-            chooserRed.gameObject.SetActive(true);  
-        } else {
+            chooserRed.gameObject.SetActive(true);
+        }
+        else
+        {
+            // Otherwise, set both chooser GameObjects to inactive
             chooserBlue.gameObject.SetActive(false);
             chooserRed.gameObject.SetActive(false);
         }
 
         // the wide if red or is blue or isnt??? Huwooga.
-        if(page.hasExpanded && trueIsBlue){
-            expandedBlue.gameObject.SetActive(true);
-            expandedRed.gameObject.SetActive(false);
-        } else if (page.hasExpanded && !trueIsBlue) {
-            expandedRed.gameObject.SetActive(true);
-            expandedBlue.gameObject.SetActive(false);
-        } else {
-            expandedBlue.gameObject.SetActive(false);
-            expandedRed .gameObject.SetActive(false);
+        if (page.hasExpanded) {
+            // Enable the blue or red expanded object based on the value of trueIsBlue
+            expandedBlue.gameObject.SetActive(trueIsBlue);
+            expandedRed.gameObject.SetActive(!trueIsBlue);
         }
-
-
-
-
+        else {
+            // Disable both expanded objects
+            expandedBlue.gameObject.SetActive(false);
+            expandedRed.gameObject.SetActive(false);
+        }
     }
 
     // using on click, the "page" will change
